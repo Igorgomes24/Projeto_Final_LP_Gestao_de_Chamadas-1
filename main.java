885b926ee3f08923109cc9e2600a7206054fe4cc
Igorgomes24 <igomes.l24@us.edu.cv>
2025-06-21 15:52:30 -0100

    Update main.java

--- main.java
+++ main.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args) {
+
+        public public static void main(String[] args) {
+            
+        }
         
     }
 }
